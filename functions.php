42b0d8ec76c83bc059886b15d5e655a4755a4f6e
PetarNovitovic <pnovitovic1@gmail.com>
2022-09-22 19:40:34 +0200

    Bootstrap added to the theme

--- functions.php
+++ functions.php
@@ -6,14 +6,22 @@
 */
 
 function lcode_enqueue_scripts() {
-    // Registering styles and scripts
-    wp_register_style( 'main-style', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+    // Registering Styles
+    wp_register_style( 'main-css', get_stylesheet_uri(), [], filemtime( get_template_directory() . '/style.css' ), 'all' );
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/src/library/css/bootstrap.min.css', [], false , 'all' );
+
+    // Registering Scripts
     wp_register_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', [], filemtime( get_template_directory() . '/assets/js/main.js' ), true );
+    wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', [ 'jquery' ], false, true );
 
 
-    // Enqueuing styles and scripts
-    wp_enqueue_style( 'main-style' );
+    // Enqueuing Styles
+    wp_enqueue_style( 'main-css' );
+    wp_enqueue_style( 'bootstrap-css' );
+
+    // Enqueuing Scripts
     wp_enqueue_script( 'main-js' );
+    wp_enqueue_script( 'bootstrap-js' );
 }
 
 add_action('wp_enqueue_scripts', 'lcode_enqueue_scripts');
