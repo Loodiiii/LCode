c9146ae4274f5660a50bf5c63f93e7ffc76e7924
PetarNovitovic <pnovitovic1@gmail.com>
2022-10-07 12:56:48 +0200

    added custom logo and title tag theme support

--- inc/classes/class-lcode-theme.php
+++ inc/classes/class-lcode-theme.php
@@ -23,9 +23,28 @@ class LCode_THEME {
     }
 
     protected function set_hooks() {
+
         /**
          * Actions.
          */
 
+        add_action('after_setup_theme', [ $this, 'setup_theme' ] );
+
     }
+
+    public function setup_theme() {
+        add_theme_support( 'title-tag' );
+
+        add_theme_support( 'custom-logo', [
+            'header-text'          => [ 'site-title', 'site-description' ],
+            'height'               => 100,
+            'width'                => 400,
+            'flex-height'          => true,
+            'flex-width'           => true,
+            'unlink-homepage-logo' => false,
+        ] );
+    }
+
 }
+
+
