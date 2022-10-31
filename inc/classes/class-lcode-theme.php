0a77aafccb3ad5622f94b980d3bb17ab85060fb3
PetarNovitovic <pnovitovic1@gmail.com>
2022-10-31 07:56:00 +0100

    header menu implemetation

--- inc/classes/class-lcode-theme.php
+++ inc/classes/class-lcode-theme.php
@@ -17,6 +17,7 @@ class LCode_THEME {
         // load classes
 
         Assets::get_instance();
+        Menus::get_instance();
 
         $this->set_hooks();
 
@@ -51,6 +52,35 @@ class LCode_THEME {
         ]);
 
         add_theme_support( 'post-thumbnails' );
+
+        add_theme_support( 'customize-selective-refresh-widgets' );
+
+        add_theme_support( 'automatic-feed-links' );
+
+        add_theme_support(
+            'html5',
+            [
+                'search-form',
+                'comment-form',
+                'comment-list',
+                'gallery',
+                'caption',
+                'script',
+                'style',
+            ]
+        );
+
+        add_editor_style( '' );
+
+        add_theme_support( 'wp-block-styles' );
+
+        add_theme_support( 'align-wide' );
+
+        global $content_width;
+        if( ! isset( $content_width ) ) {
+            $content_width = 1240;
+        }
+
 
     }
 
