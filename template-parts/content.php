6ec0472444f83f0f124b03e2796cb2f4c5f5b20d
PetarNovitovic <pnovitovic1@gmail.com>
2022-11-02 02:40:53 +0100

    content and content none created as template parts

---
+++ template-parts/content.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * Content template.
+ * 
+ * @package LCode
+ */
+
+?>
+
+<h3><?php the_title(); ?></h3>
+<div><?php the_excerpt(); ?></div>
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -13,70 +13,72 @@ $header_menus = wp_get_nav_menu_items( $header_menu_id );
 ?>
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <?php
-    if ( function_exists( 'the_custom_logo' ) ) {
-        the_custom_logo();
-    }
-  ?>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-    <?php 
-    if( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+  <div class="container">
+    <?php
+      if ( function_exists( 'the_custom_logo' ) ) {
+          the_custom_logo();
+      }
     ?>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-      <ul class="navbar-nav mr-auto">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <?php
-        foreach( $header_menus as $menu_item ) {
-          if( ! $menu_item->menu_item_parent ) {
+      <?php 
+      if( ! empty( $header_menus ) && is_array( $header_menus ) ) {
+      ?>
 
-            $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
-            $has_children = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+        <ul class="navbar-nav mr-auto">
 
-            if( ! $has_children ) {
-            ?>
+        <?php
+          foreach( $header_menus as $menu_item ) {
+            if( ! $menu_item->menu_item_parent ) {
 
-              <li class="nav-item">
-                <a class="nav-link" href="<?php echo esc_url( $menu_item->url); ?>"><?php echo esc_html( $menu_item->title ); ?></a>
-              </li>
+              $child_menu_items = $menu_class->get_child_menu_items( $header_menus, $menu_item->ID );
+              $has_children = ! empty( $child_menu_items ) && is_array( $child_menu_items );
 
-            <?php
-              } else {
-            ?>
-              <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <?php echo esc_html( $menu_item->title ); ?>
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                    <?php 
-                      foreach( $child_menu_items as $child_menu_item ) {
+              if( ! $has_children ) {
+              ?>
+
+                <li class="nav-item">
+                  <a class="nav-link" href="<?php echo esc_url( $menu_item->url); ?>"><?php echo esc_html( $menu_item->title ); ?></a>
+                </li>
+
+              <?php
+                } else {
+              ?>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <?php echo esc_html( $menu_item->title ); ?>
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <?php 
+                        foreach( $child_menu_items as $child_menu_item ) {
+                      ?>
+                        <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url); ?>">
+                          <?php echo esc_html( $child_menu_item->title ); ?>
+                        </a>
+                      <?php
+                      }
                     ?>
-                      <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url); ?>">
-                        <?php echo esc_html( $child_menu_item->title ); ?>
-                      </a>
-                    <?php
-                    }
-                  ?>
-                </div>
-              </li>
-            <?php
+                  </div>
+                </li>
+              <?php
+              }
             }
           }
-        }
+        ?>
+        </ul>
+      <?php
+      }
       ?>
-      </ul>
-    <?php
-    }
-    ?>
 
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
   </div>
 </nav>
 <?php
