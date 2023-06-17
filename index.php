5c47bcfec823a2de25091635a40a7cb9fd1c6c9c
PetarNovitovic <pnovitovic1@gmail.com>
2023-06-17 03:57:58 +0200

    Add title to entry header

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
                 if( is_home() && ! is_front_page() ) {
                     ?>
                     <header class="mb-5">
-                        <h1 class="page-title screen-reader-text">
+                        <h1 class="page-title">
                             <?php single_post_title(); ?>
                         </h1>
                     </header>
--- template-parts/components/blog/entry-header.php
+++ template-parts/components/blog/entry-header.php
@@ -6,10 +6,12 @@
  */
 
 $the_post_id = get_the_ID();
+$hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );
+$heading_class = ! empty( $hide_title ) && 'yes' === $hide_title ? 'hidden' : '';
+echo 'cao: ' . $heading_class;
+// wp_die();
 
 $has_post_thumbnail = get_the_post_thumbnail( $the_post_id );
-$hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );
-
 
 ?>
 
@@ -33,6 +35,21 @@ $hide_title = get_post_meta( $the_post_id, '_hide_page_title', true );
             </a>
         </div>
         <?php
+    }
+
+    // Title 
+    if( is_single() || is_page() ) {
+        printf(
+            '<h1 class="page-title text-dark %1$s">%2$s</h1>',
+            esc_attr( $heading_class ),
+            wp_kses_post( get_the_title() )
+        );
+    } else {
+        printf(
+            '<h2 class="entry-title mb-3"><a href="%1$s" class="text-dark">%2$s</a></h2>',
+            esc_url( get_the_permalink() ),
+            wp_kses_post( get_the_title() )
+        );
     }
     
     ?>
